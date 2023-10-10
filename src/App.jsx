82ef572b50782135445eef0b0e7c2ad9ee5f4cf1
Ralph Fang <ralphrainierfang@yahoo.com>
2023-10-10 15:42:48 -0400

    app.jsc puppies added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-
+import { useState } from 'react'
+import {puppyList} from './data.js'
 import './App.css'
 
-function App() {
-  
 
+
+function App() {
+  const [puppies, setPuppies] = useState(puppyList)
+  console.log(puppies)
   return (
     <>
-      <div>
-       
+      <div className="App">
+        {
+          puppies.map((puppy) => {
+            return <p key={puppy.id}>{puppy.name}</p>
+          })
+        }
+       <h1>Puppy</h1>
       </div>
       
     </>
