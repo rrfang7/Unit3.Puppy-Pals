2d1864139ddb1178baa6fda65baa5237d27d13fd
Ralph Fang <ralphrainierfang@yahoo.com>
2023-10-10 15:57:35 -0400

    updated feature puppies

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,28 @@ import './App.css'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList)
-  console.log(puppies)
+  const [featPupId, setFeatPupId] = useState(null)
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  console.log(featuredPup)
+ // console.log(puppies)
   return (
     <>
       <div className="App">
         {
           puppies.map((puppy) => {
-            return <p key={puppy.id}>{puppy.name}</p>
+            return <p onClick={(handleClick)=>{setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
           })
         }
        <h1>Puppy</h1>
+       {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
       </div>
       
     </>
